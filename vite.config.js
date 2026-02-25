42fb25375d53769ef32e0cd329722ca7a8c2fe95
JesusMAlba <jesusmariaalbapablo@gmail.com>
2026-02-25 19:22:17 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  server: {
+    watch: {
+      // Esta opción fuerza a Vite a detectar cambios en Windows/WSL
+      usePolling: true,
+    },
+  },
+  base: "./",
+});
